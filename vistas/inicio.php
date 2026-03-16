5c705254d2f7f20ab9154fa7546800248f9b6614
CUYORITO <silvafragosomisael@gmail.com>
2026-03-15 19:39:40 -0600

    Creacion de vistas para usuario y administrador, se dejo pendiente el navbar ya que no jala

---
+++ vistas/inicio.php
@@ -0,0 +1,14 @@
+<!-- conexion con el archivo header -->
+<?php include "header.php"; ?>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="card border-0 shadow my-5">
+            <div class="card-body p-5">
+                <h1 class="fw-light"> Inicio </h1>
+                <p class="lead"> text is null </p>
+        </div>
+    </div>
+
+<!-- Conexxion con el archivo flooter-->
+ <?php include "flooter.php"; ?>
